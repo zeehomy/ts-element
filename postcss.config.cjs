81f72e9fa10e61952e161e9d9ebcc5e46329c622
yuezh4 <yuezh4@lenovo.com>
2024-07-15 16:15:27 +0800

    feat: update add postcss-each-variables

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -5,7 +5,14 @@
 /* eslint-env node */
 module.exports = {
   plugins: [
-    require('postcss-each'),
-    require('postcss-for')
+    require('postcss-each-variables'),
+    require('postcss-each')({
+      plugins: {
+        beforeEach: [
+          require('postcss-for'),
+          require('postcss-color-mix')
+        ]
+      }
+    })
   ]
 };
